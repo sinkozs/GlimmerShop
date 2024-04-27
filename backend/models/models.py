5b2a0f4302c99f626093fc7e3b48132216408fb7
sinkozs <zsofia.sinko@outlook.com>
2024-04-27 18:14:13 +0200

    Create the database

--- backend/models/models.py
+++ backend/models/models.py
@@ -6,43 +6,37 @@ from sqlalchemy.dialects.postgresql import UUID
 from models.database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Category(Base):
+    __tablename__ = "category"
     __table_args__ = {'schema': 'public'}
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
-    email = Column(String(100), nullable=False, index=True, unique=True)
-    hashed_password = Column(String(64), nullable=False)
-    is_organiser = Column(Boolean, default=False)
-    is_verified = Column(Boolean, default=False)
-    is_active = Column(Boolean, default=False)
-    last_login = Column(DateTime, nullable=True)
-    registration_date = Column(Date, nullable=False)
-    user_events = relationship("UserEvents", back_populates="user")
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(length=200))
+
+    event_category = relationship("EventCategory", back_populates="category")
 
 
 class Organizer(Base):
-    __tablename__ = "organizers"
+    __tablename__ = "organizer"
     __table_args__ = {'schema': 'public'}
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
     email = Column(String(100), nullable=False, index=True, unique=True)
     hashed_password = Column(String(64), nullable=False)
-    is_organiser = Column(Boolean, default=True)
+    is_organizer = Column(Boolean, default=True)
     is_verified = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
     last_login = Column(DateTime, nullable=True)
     registration_date = Column(Date, nullable=False)
-    events = relationship("Events", back_populates="organiser")
+
+    event = relationship("Event", back_populates="organizer")
 
 
-class Events(Base):
-    __tablename__ = "events"
+class Event(Base):
+    __tablename__ = "event"
     __table_args__ = {'schema': 'public'}
     id = Column(Integer, primary_key=True, index=True)
-    organiser_id = Column(Integer, ForeignKey('organisers.id'))
+    organizer_id = Column(UUID(as_uuid=True), ForeignKey('public.organizer.id'))
     title = Column(String(length=100))
     event_description = Column(String(length=15000))
     event_date = Column(DateTime)
@@ -51,60 +45,62 @@ class Events(Base):
     image_path = Column(String(length=200))
     image_path2 = Column(String(length=200))
 
-    organiser = relationship("Organiser", back_populates="events")
+    organizer = relationship("Organizer", back_populates="event")
     event_category = relationship("EventCategory", back_populates="event")
-    user_events = relationship("UserEvents", back_populates="event")
-    ticket_types = relationship("TicketType", back_populates="event")
-
-    class TicketType(Base):
-        __tablename__ = "ticket_types"
-        __table_args__ = {'schema': 'public'}
-        id = Column(Integer, primary_key=True, index=True)
-        event_id = Column(Integer, ForeignKey('events_table.id'))
-        title = Column(String(length=100))
-        ticket_description = Column(String(length=200))
-        price = Column(Integer)
-        limits = Column(Integer)
-        tickets_sold = Column(Integer, default=0)
-        event = relationship("Events", back_populates="ticket_types")
-
-
-class UserEvents(Base):
-    __tablename__ = "user_events"
-    __table_args__ = {'schema': 'public'}
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    event_id = Column(Integer, ForeignKey('events_table.id'))
-
-    user = relationship("User", back_populates="user_events")
-    event = relationship("Events", back_populates="user_events")
-
-
-class Category(Base):
-    __tablename__ = "categories"
-    __table_args__ = {'schema': 'public'}
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(length=200))
-
-    event_category = relationship("EventCategory", back_populates="category")
+    user_event = relationship("UserEvent", back_populates="event")
+    ticket_type = relationship("TicketType", back_populates="event")
 
 
 class EventCategory(Base):
-    __tablename__ = "event_categories"
+    __tablename__ = "event_category"
     __table_args__ = {'schema': 'public'}
     id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer, ForeignKey('events_table.id'))
-    category_id = Column(Integer, ForeignKey('categories.id'))
+    event_id = Column(Integer, ForeignKey('public.event.id'))
+    category_id = Column(Integer, ForeignKey('public.category.id'))
 
-    event = relationship("Events", back_populates="event_category")
+    event = relationship("Event", back_populates="event_category")
     category = relationship("Category", back_populates="event_category")
 
 
-class Cart(Base):
+class TicketType(Base):
+    __tablename__ = "ticket_type"
     __table_args__ = {'schema': 'public'}
-    __tablename__ = "carts"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    event_id = Column(Integer, ForeignKey('public.event.id'))
+    title = Column(String(length=100))
+    ticket_description = Column(String(length=200))
+    price = Column(Integer)
+    limits = Column(Integer)
+    tickets_sold = Column(Integer, default=0)
+
+    event = relationship("Event", back_populates="ticket_type")
+
+
+class User(Base):
+    __tablename__ = "user"
+    __table_args__ = {'schema': 'public'}
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(100), nullable=False, index=True, unique=True)
+    hashed_password = Column(String(64), nullable=False)
+    is_organizer = Column(Boolean, default=False)
+    is_verified = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    last_login = Column(DateTime, nullable=True)
+    registration_date = Column(Date, nullable=False)
+
+    user_event = relationship("UserEvent", back_populates="user")
+    cart = relationship("Cart", back_populates="user")
+
+
+class Cart(Base):
+    __tablename__ = "cart"
+    __table_args__ = {'schema': 'public'}
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('public.user.id'))
+
+    user = relationship("User", back_populates="cart")
     cart_items = relationship("CartItem", back_populates="cart")
 
 
@@ -113,7 +109,19 @@ class CartItem(Base):
     __table_args__ = {'schema': 'public'}
     id = Column(Integer, primary_key=True, index=True)
     quantity = Column(Integer)
-    cart_id = Column(Integer, ForeignKey('carts.id'))
-    ticket_type_id = Column(Integer, ForeignKey('ticket_types.id'))
-    cart = relationship("Cart", back_populates="cart_items")
+    cart_id = Column(Integer, ForeignKey('public.cart.id'))
+    ticket_type_id = Column(Integer, ForeignKey('public.ticket_type.id'))
 
+    cart = relationship("Cart", back_populates="cart_item")
+
+
+class UserEvent(Base):
+    __tablename__ = "user_event"
+    __table_args__ = {'schema': 'public'}
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('public.user.id'))
+    event_id = Column(Integer, ForeignKey('public.event.id'))
+
+    user = relationship("User", back_populates="user_event")
+    event = relationship("Event", back_populates="user_event")
+    
