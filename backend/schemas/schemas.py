1fa0668ac0c3c025ea649790033845c173d840ba
sinkozs <zsofia.sinko@outlook.com>
2024-06-21 14:21:30 +0200

    update schemas

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -1,42 +1,39 @@
 from typing import Optional
-from dataclasses import dataclass
-from pydantic import BaseModel, EmailStr, StringConstraints
+from pydantic import BaseModel, EmailStr, Field
 from typing_extensions import Annotated
 
 
 class UserCreate(BaseModel):
-    first_name: Annotated[str, StringConstraints(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$', strict=True)]
-    last_name: Annotated[str, StringConstraints(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$', strict=True)]
-    password: Annotated[str, StringConstraints(strip_whitespace=True, max_length=64, min_length=8, strict=True)]
+    first_name: Annotated[str, Field(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$')]
+    last_name: Annotated[str, Field(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$')]
+    password: Annotated[str, Field(strip_whitespace=True, max_length=64, min_length=8)]
     email: EmailStr
     is_seller: bool
 
 
-@dataclass
-class UserUpdate:
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
+class UserUpdate(BaseModel):
+    first_name: Optional[Annotated[str, Field(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$')]] = None
+    last_name: Optional[Annotated[str, Field(strip_whitespace=True, max_length=25, min_length=3, pattern=r'^[a-zA-ZáÁéÉíÍóÓöÖüÜőŐúÚ-]+$')]] = None
     email: Optional[EmailStr] = None
-    password: Optional[str] = None
+    password: Optional[Annotated[str, Field(strip_whitespace=True, max_length=64, min_length=8)]] = None
 
 
 class ProductCreate(BaseModel):
-    name: Annotated[str, StringConstraints(max_length=100, strict=True)]
-    description: Annotated[str, StringConstraints(max_length=150000, strict=True)]
-    price: int
-    stock_quantity: int
+    name: Annotated[str, Field(max_length=100)]
+    description: Annotated[str, Field(max_length=150000)]
+    price: Annotated[int, Field(gt=0)]
+    stock_quantity: Annotated[int, Field(ge=0)]
     material: str
     color: str
     image_path: str
     image_path2: str
 
 
-@dataclass
-class ProductUpdate:
-    name: Optional[str] = None
-    description: Optional[str] = None
-    price: Optional[int] = None
-    stock_quantity: Optional[int] = None
+class ProductUpdate(BaseModel):
+    name: Optional[Annotated[str, Field(max_length=100)]] = None
+    description: Optional[Annotated[str, Field(max_length=150000)]] = None
+    price: Optional[Annotated[int, Field(gt=0)]] = None
+    stock_quantity: Optional[Annotated[int, Field(ge=0)]] = None
     material: Optional[str] = None
     color: Optional[str] = None
     image_path: Optional[str] = None
