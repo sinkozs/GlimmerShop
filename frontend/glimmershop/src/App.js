4133944b18661f646076b171f6a9cfad784f1b84
sinkozs <zsofia.sinko@outlook.com>
2024-07-27 13:51:10 +0200

    add new React component for category-specific product listings

--- frontend/glimmershop/src/App.js
+++ frontend/glimmershop/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import logo from "./logo.svg";
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './routes/Home';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./routes/Home";
+const ProductDetails = lazy(() => import("./routes/ProductDetails"));
+const ProductsByCategory = lazy(() => import("./routes/ProductsByCategory"));
 
 function App() {
   return (
@@ -14,6 +17,23 @@ function App() {
         <div className="content-container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route
+              path="/products/:product_id"
+              element={
+                <Suspense fallback={<div>Loading Product Details...</div>}>
+                  <ProductDetails />
+                </Suspense>
+              }
+            />
+
+            <Route
+              path="categories/:category_name"
+              element={
+                <Suspense fallback={<div>Loading Products...</div>}>
+                  <ProductsByCategory />
+                </Suspense>
+              }
+            />
           </Routes>
         </div>
         <Footer />
---
+++ frontend/glimmershop/src/routes/ProductsByCategory.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState} from 'react';
+import { Container, Card } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from 'axios';
+
+import "../App.css";
+import "../styles/Home.css";
+import "../styles/TrendingJewelry.css";
+
+function TrendingJewelry() {
+  const [categoryData, setCategoryData] = useState(null);
+  const [categoryProducts, setCategoryProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const { category_name } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCategoryAndProducts = async () => {
+      try {
+        // Fetch category data
+        const categoryResponse = await axios.get(`http://localhost:8000/categories/category-by-identifier?category_identifier=${category_name}`);
+        setCategoryData(categoryResponse.data);
+
+        // Fetch products using the category ID
+        const productsResponse = await axios.get(`http://localhost:8000/categories/products-by-category/?category_id=${categoryResponse.data.category_record.id}`);
+        setCategoryProducts(productsResponse.data);
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data. Please try again later.");
+      }
+    };
+
+    fetchCategoryAndProducts();
+  }, [category_name]);
+
+  const handleCardClick = (productId) => {
+    console.log("Navigating to product with ID:", productId);
+    navigate(`/products/${productId}`);
+  };
+
+  
+  if (!categoryData || !categoryProducts) {
+    return <div>Loading...</div>;
+  }
+
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <Container fluid className="trending-section-wrapper">
+    {error && (
+      <p style={{ color: "red" }}>Error fetching products: {error.message}</p>
+    )}
+    <Container fluid className="trending-section-grid">
+      {categoryProducts.length > 0 ? (
+        categoryProducts.map((product, idx) => (
+          <Card
+            key={idx}
+            className="trending-card"
+            onClick={() => handleCardClick(product.id)}
+          >
+            <Card.Body className="trending-card-body">
+              <div className="image-container">
+                <Card.Img
+                  variant="top"
+                  className="trending-card-image"
+                  src={`http://localhost:8000/${product.image_path}`}
+                />
+                <Card.Img
+                  variant="top"
+                  className="trending-card-hover"
+                  src={`http://localhost:8000/${product.image_path2}`}
+                />
+              </div>
+            </Card.Body>
+            <Card.Footer className="card-footer">
+              <Card.Text className="footer-text">{product.name}</Card.Text>
+              <Card.Text className="footer-text-price">
+                ${product.price}
+              </Card.Text>
+            </Card.Footer>
+          </Card>
+        ))
+      ) : (
+        <p>No products available.</p>
+      )}
+    </Container>
+  </Container>
+  );
+}
+
+export default TrendingJewelry;
