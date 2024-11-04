3f517afbd4b60e16fa7758352f5140a1daf2942c
sinkozs <zsofia.sinko@outlook.com>
2024-11-04 20:24:13 +0100

    refactor CartContext to use useReducer for managing cart state

--- frontend/glimmershop/src/context/CartContext.js
+++ frontend/glimmershop/src/context/CartContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useReducer, useEffect } from "react";
 import axios from "axios";
 import Modal from "../components/Modal";
 import { Container } from "react-bootstrap";
@@ -8,95 +8,125 @@ const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
-export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState(() => {
-    const localData = localStorage.getItem("cart");
-    return localData ? JSON.parse(localData) : [];
-  });
-  const [stockQuantity, setStockQuantity] = useState(null);
-  const [showModal, setShowModal] = useState(false);
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      const existingItemIndex = state.cart.findIndex(
+        (i) => i.id === action.payload.id
+      );
+      if (existingItemIndex > -1) {
+        const updatedCart = [...state.cart];
+        updatedCart[existingItemIndex].quantity += action.payload.quantity;
+        return { ...state, cart: updatedCart };
+      } else {
+        return { ...state, cart: [...state.cart, action.payload] };
+      }
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
+    case "INCREASE_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload.itemId &&
+          item.quantity < state.stockQuantity
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+        showModal: state.cart.some(
+          (item) =>
+            item.id === action.payload.itemId &&
+            item.quantity >= state.stockQuantity
+        ),
+      };
+    case "DECREASE_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload
+            ? { ...item, quantity: Math.max(0, item.quantity - 1) }
+            : item
+        ),
+      };
+    case "SET_STOCK_QUANTITY":
+      return { ...state, stockQuantity: action.payload };
+    case "DELETE_CART":
+      return { ...state, cart: [] };
+    case "CLOSE_MODAL":
+      return { ...state, showModal: false };
+    default:
+      return state;
+  }
+};
 
-  const closeModal = () => {
-    setShowModal(false);
-  };
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, {
+    cart: JSON.parse(localStorage.getItem("cart")) || [],
+    stockQuantity: null,
+    showModal: false,
+  });
 
   useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   const addToCart = async (item) => {
     try {
       const product = await axios.get(
         `${config.BACKEND_BASE_URL}/products/${item.id}`
       );
-
       const newItem = {
         ...item,
         image_path: product.data.image_path,
+        quantity: item.quantity,
       };
-      setStockQuantity(product.data.stock_quantity);
-
-      setCart((prevCart) => {
-        const existingItemIndex = prevCart.findIndex(
-          (i) => i.id === newItem.id
-        );
-        if (existingItemIndex > -1) {
-          const newCart = [...prevCart];
-          newCart[existingItemIndex].quantity += newItem.quantity;
-          return newCart;
-        } else {
-          return [...prevCart, newItem];
-        }
+      dispatch({
+        type: "SET_STOCK_QUANTITY",
+        payload: product.data.stock_quantity,
       });
+      dispatch({ type: "ADD_TO_CART", payload: newItem });
     } catch (error) {
       console.error("Failed to add product to cart:", error);
     }
   };
 
   const removeFromCart = (itemId) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+    dispatch({ type: "REMOVE_FROM_CART", payload: itemId });
   };
 
   const increaseQuantity = (itemId) => {
-    setCart((prevCart) => {
-      const item = prevCart.find((item) => item.id === itemId);
-      if (item && item.quantity + 1 <= stockQuantity) {
-        return prevCart.map((item) =>
-          item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        setShowModal(true);
-        return prevCart;
-      }
-    });
+    dispatch({ type: "INCREASE_QUANTITY", payload: { itemId } });
   };
 
   const decreaseQuantity = (itemId) => {
-    setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.id === itemId
-          ? { ...item, quantity: Math.max(0, item.quantity - 1) }
-          : item
-      )
-    );
+    dispatch({ type: "DECREASE_QUANTITY", payload: itemId });
   };
 
   const calculateSubtotal = () => {
-    return cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    return state.cart.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
   };
 
   const calculateTotalCartItemCount = () => {
-    return cart.reduce((acc, item) => acc + item.quantity, 0);
+    return state.cart.reduce((acc, item) => acc + item.quantity, 0);
   };
 
   const deleteCart = () => {
-    setCart([]);
+    dispatch({ type: "DELETE_CART" });
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return (
     <CartContext.Provider
       value={{
-        cart,
+        cart: state.cart,
         addToCart,
         removeFromCart,
         increaseQuantity,
@@ -107,11 +137,11 @@ export const CartProvider = ({ children }) => {
       }}
     >
       {children}
-      <Modal show={showModal} onClose={closeModal} title="Sorry!">
+      <Modal show={state.showModal} onClose={closeModal} title="Sorry!">
         <Container>
-          We currently have only {stockQuantity}{" "}
-          {stockQuantity === 1 ? "unit" : "units"} of this product in stock.
-          Please adjust the quantity in your cart accordingly.
+          We currently have only {state.stockQuantity}{" "}
+          {state.stockQuantity === 1 ? "unit" : "units"} of this product in
+          stock. Please adjust the quantity in your cart accordingly.
         </Container>
       </Modal>
     </CartContext.Provider>
