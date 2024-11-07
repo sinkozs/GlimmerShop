9f9d77a107fd0be763147494380da4a62b78ad12
sinkozs <zsofia.sinko@outlook.com>
2024-11-07 20:02:31 +0100

    remove description column from categories

--- backend/models/models.py
+++ backend/models/models.py
@@ -11,7 +11,6 @@ class Category(Base):
     __table_args__ = {'schema': 'public'}
     id = Column(Integer, primary_key=True, index=True)
     category_name = Column(String(length=200))
-    category_description = Column(String(length=200))
 
     product_category = relationship("ProductCategory", back_populates="category")
 
