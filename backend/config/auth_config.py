3a2a12dcef2c1d004cf5d2a514e263e52e0f29d5
sinkozs <zsofia.sinko@outlook.com>
2025-03-26 10:19:01 +0100

    use the Argon2 hashing algorithm instead of bcrypt

--- backend/config/auth_config.py
+++ backend/config/auth_config.py
@@ -1,8 +1,10 @@
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from argon2 import PasswordHasher
 
 jwt_algorithm = "RS256"
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+bcrypt_context = CryptContext(schemes=["argon2"], deprecated="auto")
+password_hasher = PasswordHasher()
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 http_only_auth_cookie = "glimmershop_successful_login"
 token_expiry_minutes = 20
